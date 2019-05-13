79fdaa8450d01935bf5f6848028e732859620c3d
Mo Bitar <me@bitar.io>
2019-05-13 12:23:25 -0500

    Use FilesafeEmbed, v2.0.0

--- app/App.js
+++ app/App.js
@@ -1,16 +1,90 @@
 import React from 'react';
-import Home from './components/Home';
-import _ from 'lodash';
+import FilesafeEmbed from "filesafe-embed";
+import Filesafe from "filesafe-js";
+import ComponentManager from 'sn-components-api';
+
+const BaseHeight = 53;
+const MessageHavingHeight = 28;
+const PerMessageHeight = 22;
+const ExpandedHeight = 305;
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {expanded: false};
+  }
+
+  componentDidMount() {
+    this.componentManager = new ComponentManager(null, () => {
+      // On ready and permissions authorization
+      document.documentElement.classList.add(this.componentManager.platform);
+    });
+
+    this.filesafe = new Filesafe({componentManager: this.componentManager});
+    this.fsObserver = this.filesafe.addDataChangeObserver(() => {
+      this.recomputeHeight();
+    })
+
+    this.componentManager.streamContextItem((incomingNote) => {
+      let itemClass = Filesafe.getSFItemClass();
+      let noteModel = new itemClass(incomingNote);
+      this.filesafe.setCurrentNote(noteModel);
+    });
+
+    let mountPoint = document.getElementById("embed");
+    FilesafeEmbed.FilesafeEmbed.renderInElement(mountPoint, this.filesafe);
+
+    this.recomputeHeight();
+  }
+
+  recomputeHeight() {
+    var totalHeight = BaseHeight;
+
+    var credentials = this.filesafe.getAllCredentials();
+    if(credentials.length == 0) {
+      totalHeight += PerMessageHeight;
+    }
+
+    var integrations = this.filesafe.getAllIntegrations();
+    if(integrations.length == 0) {
+      totalHeight += PerMessageHeight;
+    }
+
+    if(integrations.length == 0 || credentials.length == 0) {
+      totalHeight += MessageHavingHeight;
+    }
+
+    if(this.state.expanded) {
+      totalHeight = ExpandedHeight;
+    }
+
+    this.componentManager.setSize("container", "100%", totalHeight);
+  }
+
+  toggleHeight() {
+    if(this.state.expanded) {
+      this.setHeightCollapsed();
+    } else {
+      this.setHeightExpanded();
+    }
+  }
+
+  setHeightExpanded() {
+    this.setState({expanded: true}, this.recomputeHeight)
+  }
+
+  setHeightCollapsed() {
+    this.setState({expanded: false}, this.recomputeHeight);
   }
 
   render() {
     return (
-      <div>
-        <Home />
+      <div id="root">
+        <div id="embed">
+        </div>
+        <div id="expand-button" className="sk-button neutral no-border" onClick={this.toggleHeight.bind(this)}>
+          <div className="sk-label">{this.state.expanded ? "Collapse" : "Expand"}</div>
+        </div>
       </div>
     );
   }
